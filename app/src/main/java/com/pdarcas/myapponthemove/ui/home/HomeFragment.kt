44514651d165ed2c9cfd5648ac1095dc4f1c48df
Pierre <pierre.darcas@lacatholille.fr>
2021-04-07 16:28:55 +0200

    ✨virgin map

--- app/src/main/java/com/pdarcas/myapponthemove/ui/home/HomeFragment.kt
+++ app/src/main/java/com/pdarcas/myapponthemove/ui/home/HomeFragment.kt
@@ -1,13 +1,21 @@
 package com.pdarcas.myapponthemove.ui.home
 
+import android.R
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Transformations.map
 import androidx.lifecycle.ViewModelProvider
 import com.pdarcas.myapponthemove.databinding.FragmentHomeBinding
 import com.pdarcas.myapponthemove.utils.fragmentAutoCleared
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.MapView
+
+
 
 class HomeFragment : Fragment() {
 
@@ -22,8 +30,25 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding= FragmentHomeBinding.inflate(inflater,container,false)
+        _binding= FragmentHomeBinding.inflate(inflater, container, false)
 
         return _binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        org.osmdroid.config.Configuration.getInstance().userAgentValue = requireContext().packageName
+        _binding.map.apply {
+            zoomController.setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT)
+            setMultiTouchControls(true)
+            val startPoint = GeoPoint(48.13, -1.63)
+            val mapController = controller
+            mapController.setZoom(3.0)
+            mapController.setCenter(startPoint)
+            this.isTilesScaledToDpi=true
+        }
+
+    }
+
+
 }
