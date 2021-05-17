6a3ba38237b2fe5b1c9e0edfba01e4d51942adaf
Julien Dudek <julien.dudek@gmail.com>
2021-05-17 23:44:14 +0200

    📌 ⬆️

--- app/src/main/java/com/pdarcas/myapponthemove/ui/activities/MainActivity.kt
+++ app/src/main/java/com/pdarcas/myapponthemove/ui/activities/MainActivity.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mvince.m1test.utils.activityViewBinding
-import com.pdarcas.myapponthemove.R
+
 import com.pdarcas.myapponthemove.databinding.MainActivityBinding
+
 import com.pdarcas.myapponthemove.ui.home.ModalBottomSheetFragmentMenu
 
 class MainActivity : AppCompatActivity() {
