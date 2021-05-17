93cbb58f2d9ed14389a9199c6577a4f77e735db4
Julien Dudek <julien.dudek@gmail.com>
2021-05-17 22:24:28 +0200

    🔥 ajout activityDataBinding

--- app/src/main/java/com/pdarcas/myapponthemove/MainActivity.kt
+++ app/src/main/java/com/pdarcas/myapponthemove/MainActivity.kt
@@ -2,25 +2,40 @@ package com.pdarcas.myapponthemove
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mvince.m1test.utils.activityViewBinding
+import com.pdarcas.myapponthemove.R
+import com.pdarcas.myapponthemove.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by activityViewBinding(MainActivityBinding::inflate)
+
+    private lateinit var mNavController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navView: BottomNavigationView = binding.navView
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        mNavController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+        ))
+
+
+        setupActionBarWithNavController(mNavController, appBarConfiguration)
+        navView.setupWithNavController(mNavController)
     }
 }
+
+
