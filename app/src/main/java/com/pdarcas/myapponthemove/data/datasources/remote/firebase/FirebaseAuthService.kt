8c9fa8ef6ca18cc8f966cd5352378a42ca7ffe9d
Morgan Lombard <morgan.lombard@protonmail.com>
2021-05-18 10:01:47 +0200

    maj firebase dependencies + FirebaseAuthService.kt

---
+++ app/src/main/java/com/pdarcas/myapponthemove/data/datasources/remote/firebase/FirebaseAuthService.kt
@@ -0,0 +1,16 @@
+package com.pdarcas.myapponthemove.data.datasources.remote.firebase
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class FirebaseAuthService {
+
+    private val auth: FirebaseAuth = Firebase.auth
+
+    fun getCurrentFirebaseUser() = auth.currentUser
+
+    fun signIn(email: String, password: String) = auth.signInWithEmailAndPassword(email, password)
+
+    fun signUp(email: String, password: String) = auth.createUserWithEmailAndPassword(email, password)
+}
