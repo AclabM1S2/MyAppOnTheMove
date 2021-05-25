1cc4adc4857f262d33456326fa850b2dba58c711
Julien Dudek <julien.dudek@gmail.com>
2021-05-25 16:21:28 +0200

    ajout binding sur modal

--- app/src/main/java/com/pdarcas/myapponthemove/ui/fragments/home/ModalBottomSheetFragmentMenu.kt
+++ app/src/main/java/com/pdarcas/myapponthemove/ui/fragments/home/ModalBottomSheetFragmentMenu.kt
@@ -7,11 +7,14 @@ import android.view.ViewGroup
 import android.widget.Button
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.pdarcas.myapponthemove.R
+import com.pdarcas.myapponthemove.databinding.FragmentBottomModalBinding
+import com.pdarcas.myapponthemove.utils.fragmentAutoCleared
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class ModalBottomSheetFragmentMenu: BottomSheetDialogFragment() {
 
     private val model: HomeViewModel by sharedViewModel()
+    private var _binding: FragmentBottomModalBinding by fragmentAutoCleared()
     private  lateinit var btnNaviguer: Button
     private lateinit var btnPosition: Button
     private lateinit var btnCharger: Button
@@ -22,17 +25,17 @@ class ModalBottomSheetFragmentMenu: BottomSheetDialogFragment() {
         }
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view=inflater.inflate(R.layout.modal_fragment_menu,container,false)
 
-        return view
+        _binding= FragmentBottomModalBinding.inflate(inflater, container, false)
+        return _binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btnPosition=view.findViewById(R.id.btn_position)
-        btnCharger = view.findViewById(R.id.btn_charger)
-        btnNaviguer=view.findViewById(R.id.btn_naviguer)
+        btnPosition=_binding.btnPosition
+        btnCharger = _binding.btnCharger
+        btnNaviguer=_binding.btnNaviguer
         btnPosition?.setOnClickListener { model.positionUser.value=true
             dismiss()}
         btnNaviguer?.setOnClickListener { model.actionNaviguer.value=true
