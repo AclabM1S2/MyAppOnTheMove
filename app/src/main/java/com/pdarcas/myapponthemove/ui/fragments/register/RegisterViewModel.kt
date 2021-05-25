9e482a1da95efe018767aeaff4e10dd3a84f75f8
Morgan Lombard <morgan.lombard@protonmail.com>
2021-05-25 09:57:40 +0200

    udapte AndroidManifest.xml and BaseApplication.kt

--- app/src/main/java/com/pdarcas/myapponthemove/ui/fragments/register/RegisterViewModel.kt
+++ app/src/main/java/com/pdarcas/myapponthemove/ui/fragments/register/RegisterViewModel.kt
@@ -1,7 +1,5 @@
 package com.pdarcas.myapponthemove.ui.fragments.register
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.pdarcas.myapponthemove.data.repositories.AuthRepository
 
