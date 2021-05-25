2d3f836d7c783ac8c44323ec52882708b6a0b665
Morgan Lombard <morgan.lombard@protonmail.com>
2021-05-25 09:04:38 +0200

    add signout and logs to AuthRepository.kt

--- app/src/main/java/com/pdarcas/myapponthemove/data/repositories/AuthRepository.kt
+++ app/src/main/java/com/pdarcas/myapponthemove/data/repositories/AuthRepository.kt
@@ -1,33 +1,40 @@
 package com.pdarcas.myapponthemove.data.repositories
 
+import android.content.ContentValues
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseUser
 import com.pdarcas.myapponthemove.data.datasources.remote.firebase.FirebaseAuthService
 
-class AuthRepository(val firebaseAuthService: FirebaseAuthService) {
+class AuthRepository(private val firebaseAuthService: FirebaseAuthService) {
 
     fun getCurrentFirebaseUser() = firebaseAuthService.getCurrentFirebaseUser()
 
     fun signIn(email: String, password: String): MutableLiveData<FirebaseUser?> {
         val response = MutableLiveData<FirebaseUser?>()
         firebaseAuthService.signIn(email, password)
-                .addOnSuccessListener {
-                    response.value = it.user
-                }.addOnFailureListener {
-
-                }
+            .addOnSuccessListener {
+                Log.d(ContentValues.TAG, "connectUserWithEmail:success")
+                response.value = it.user
+            }.addOnFailureListener { task ->
+                Log.w(ContentValues.TAG, "connectUserWithEmail:failure", task.cause)
+            }
         return response
     }
 
     fun signUp(email: String, password: String): MutableLiveData<FirebaseUser?> {
         val response = MutableLiveData<FirebaseUser?>()
         firebaseAuthService.signUp(email, password)
-                .addOnSuccessListener {
-                    response.value = it.user
-                }.addOnFailureListener {
-                    Log.d("ERREUR SignUp", it.message.toString())
-                }
+            .addOnSuccessListener {
+                Log.d(ContentValues.TAG, "createUserWithEmail:success")
+                response.value = it.user
+            }.addOnFailureListener { task ->
+                Log.w(ContentValues.TAG, "createUserWithEmail:failure", task.cause)
+            }
         return response
+    }
+
+    fun signOut() {
+        firebaseAuthService.signOut()
     }
 }
