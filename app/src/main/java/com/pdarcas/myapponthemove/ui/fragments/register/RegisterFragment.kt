637276999d501ef2db92ad9b7a47b72d21a2b144
Morgan Lombard <morgan.lombard@protonmail.com>
2021-05-25 09:15:43 +0200

    added RegisterFragment.kt and RegisterViewModel.kt

---
+++ app/src/main/java/com/pdarcas/myapponthemove/ui/fragments/register/RegisterFragment.kt
@@ -0,0 +1,92 @@
+package com.pdarcas.myapponthemove.ui.fragments.register
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.Observer
+import com.pdarcas.myapponthemove.ui.activities.MainActivity
+import com.pdarcas.myapponthemove.utils.fragmentAutoCleared
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding by fragmentAutoCleared()
+    private val registerViewModel: RegisterViewModel by viewModel()
+
+    private val permissionResultLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return _binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val emailRegister = _binding.etEmailRegister
+        val passwordRegister = _binding.etPasswordRegister
+        val passwordConfirmRegister = _binding.etPasswordConfirmRegister
+
+        _binding.btnRegister.setOnClickListener {
+            when {
+                TextUtils.isEmpty(emailRegister.text.toString().trim {
+                    it <= ' '
+                }) -> {
+                    Toast.makeText(requireActivity(), "Please enter email.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                !Patterns.EMAIL_ADDRESS.matcher(emailRegister.text.toString()).matches() -> {
+                    Toast.makeText(requireActivity(), "Invalid email format", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                (passwordRegister.text.toString() != passwordConfirmRegister.text.toString()) -> {
+                    Toast.makeText(requireActivity(), "Passwords don't match", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                TextUtils.isEmpty(passwordRegister.toString().trim {
+                    it <= ' '
+                }) -> {
+                    Toast.makeText(requireActivity(), "Please enter password.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                passwordRegister.text.length < 6 -> {
+                    Toast.makeText(requireActivity(), "PPassword too short, enter minimum 6 characters!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                else -> {
+                    registerViewModel.signUp(
+                        emailRegister.text.toString(),
+                        passwordRegister.text.toString()
+                    )
+                        .observe(viewLifecycleOwner, Observer {
+                            it?.let {
+                                val intent = Intent(requireActivity(), MainActivity::class.java)
+                                intent.flags =
+                                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                startActivity(intent)
+                                activity?.finish()
+                            } //TODO  IF ERROR DISPLAY TOAST
+                        })
+                }
+            }
+        }
+    }
+}
