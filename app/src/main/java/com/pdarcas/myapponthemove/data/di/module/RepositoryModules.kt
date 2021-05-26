c434be285abfb3f6a087c3c7a668f09f42b97011
Morgan Lombard <morgan.lombard@protonmail.com>
2021-05-26 19:58:55 +0200

    add StorageRepository to modules to make it singleton

--- app/src/main/java/com/pdarcas/myapponthemove/data/di/module/RepositoryModules.kt
+++ app/src/main/java/com/pdarcas/myapponthemove/data/di/module/RepositoryModules.kt
@@ -1,12 +1,17 @@
 package com.pdarcas.myapponthemove.data.di.module
 
+import android.content.Context
 import com.pdarcas.myapponthemove.data.datasources.remote.firebase.FirebaseAuthService
 import com.pdarcas.myapponthemove.data.repositories.AuthRepository
+import com.pdarcas.myapponthemove.data.repositories.StorageRepository
 import org.koin.dsl.module
 
 object RepositoryModules {
     val repositories = module {
         fun createAuthRepository(firebaseAuthService: FirebaseAuthService) = AuthRepository(firebaseAuthService)
+        fun createStorageRepository(context: Context) = StorageRepository(context)
+
+        single { createAuthRepository(get()) }
         single { createAuthRepository(get()) }
     }
 }
